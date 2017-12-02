f3c45becbf5128af3a986bf7e267d7031835ee19
aero <aakash30jan@gmail.com>
2017-12-02 21:40:04 +0100

    Reinitialized Files

--- src/mesh_formats/hdf/HDF_test_v1.3.py
+++ src/mesh_formats/hdf/HDF_test_v1.3.py
@@ -4,14 +4,15 @@ raw_mesh= np.loadtxt('raw_mesh.data')
 length=raw_mesh.shape[0]
 
 print 'Creating Datasets'
-velocity = raw_mesh[:,0:3]
-position = raw_mesh[:,3:6]
-pressure = raw_mesh[:,6]
+node = raw_mesh[:,0]
+velocity = raw_mesh[:,1:4]
+position = raw_mesh[:,4:7]
+pressure = raw_mesh[:,7]
 
 
 
 print 'Writing 3 Dimensional Vectors'
-hdf5 = h5py.File('mesh.hdf5', 'w')
+hdf5 = h5py.File('mesh_v2.hdf5', 'w')
 dataset_3d= hdf5.create_dataset('Vectors3D', (length,),
                               dtype=[('Position', '<f8', 3),
                                      ('Velocity', '<f8', 3)])
@@ -21,8 +22,10 @@ dataset_3d['Velocity'] = velocity
 
 print 'Writing 1 Dimensional Scalars'
 dataset_1d= hdf5.create_dataset('Scalars', (length,),
-                              dtype=[('Pressure', '<f8')])
+                              dtype=[('Pressure', '<f8'),
+                                     ('Node','<f8')])
 dataset_1d['Pressure'] = pressure
+dataset_1d['Node'] = node
 
 hdf5.close()
 
