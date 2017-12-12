29616bf3be87482b06afb3651840559490588e29
Dianafm15 <dimk_32@hotmail.com>
2017-12-12 01:35:26 +0100

    VTK Write Files

---
+++ src/mesh_formats/vtk/Bipin/digit_to_ch.f90
@@ -0,0 +1,19 @@
+subroutine digit_to_ch ( digit, ch )
+
+  implicit none
+
+  character ch
+  integer ( kind = 4 ) digit
+
+  if ( 0 <= digit .and. digit <= 9 ) then
+
+    ch = achar ( digit + 48 )
+
+  else
+
+    ch = '*'
+
+  end if
+ 
+  return
+end
