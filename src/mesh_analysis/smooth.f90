b44ebf0b850f282e4be5fd5bef9c7d7678dabff3
prosunr <33026493+prosunr@users.noreply.github.com>
2017-12-15 17:10:46 +0100

    Update smooth.f90

--- src/mesh_analysis/smooth.f90
+++ src/mesh_analysis/smooth.f90
@@ -1,56 +1,39 @@
 program smoothness
+use rat
 implicit none
 integer,parameter :: n=3
-real(8) :: gam, bet
-real(8) :: optc, K, P, W
-real(8), dimension(0:n**2-1) :: L
+real(8) :: alpha, beta, gam, phi, smth
+type(Point) :: K, P, W 
+type(point), dimension(0:n**2-1) :: L
 integer :: ia, ib
 
 call grid
-
-gam = W-P
-bet = P-K
-optc = gam/bet
+alpha = W%x-P%x 
+beta = P%x-K%x
+gam =  W%y-P%y 
+phi = P%x-K%x
 
 do ib=0, n**2-n-2, n
-do ia=1,n-1
-K = L(ia+ib)
-P = L(ia+ib+1)
-W = L(ia+ib+3)
+do ia=1,n-2
+
+K%x = L(ia+ib)%x
+K%y = L(ia+ib)%y
+P%x = L(ia+ib+1)%x
+P%y = L(ia+ib+1)%y
+W%x = L(ia+ib+n)%x
+W%y = L(ia+ib+n)%y
 
 print*, ia, ib
 
-if (gam>bet) then  
-optc= 1.2		!'optc' is ratio of 'gam' and 'bet'
+
+if (alpha>=(beta*1.2)) then  
+smth= (alpha/beta) 	
+
 end if
 
-!print*, K%x, P%y, W%x
+print*, smth
 
 end do
 end do
 print*, L
-
-contains
-subroutine grid
-implicit none
-
-real(8) :: x,y
-integer :: n
-integer :: i,j   
-real(8), parameter :: a=2,b=2,c=1 
-real(8) :: hx, hy, dble
-real(8), dimension(0:3**2-1) :: L
-  
-hx = a / dble(n-1)
-
-do i=1, n-2
-hy = (b-c*i*hx/a) / dble(n-1)
-do j=1, n-2
-L(i*n+j) = i*hx
-
-L(i*n+j) = j*hy+c*i*hx/a
-enddo
-enddo
-!print*, n
-end subroutine grid
 end program smoothness
