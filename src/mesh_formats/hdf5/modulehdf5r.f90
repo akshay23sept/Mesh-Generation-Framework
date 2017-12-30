2ba8fcbc364db4d110636ce36a373bb1270ae3a9
aero <aakash30jan@gmail.com>
2017-12-30 18:48:53 +0100

    minor changes

---
+++ src/mesh_formats/hdf5/modulehdf5r.f90
@@ -0,0 +1,12 @@
+! this module is used as as the the module for running requirs a different command to compile the program
+
+module hdfwrite
+IMPLICIT NONE
+contains
+subroutine hdf5wrt
+print*,'creating hdf5 file'
+call system("gfortran -c precision.f90")! to compile the precision.f90 to compile the module of data type precision
+call system("hdf5/bin/h5fc precision.o hdf5write.f90 -o data") ! to compile the main program
+call system("./data")! running the output file to generate hdf5 file
+end subroutine hdf5wrt
+end module hdfwrite
