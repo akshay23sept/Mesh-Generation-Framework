6dd02d73b896d29c69fc98e1efffebae29ff7fab
ahmed <ahmed.emad2020010@gmail.com>
2018-01-04 20:46:58 +0100

    Reinitialized Files

---
+++ src/mesh_analysis/astest_ahmed.f90
@@ -0,0 +1,10 @@
+program astest
+use ratio                                  ! ratio is the aspect ratio module 
+implicit none
+
+real(8):: maxi_as, as
+call test1 (maxi_as)
+
+print*, 'the maximum aspect ratio=', maxi_as            ! the maximum as 'Aspect ratio' of one cell of the mesh
+!print*, 'aspect ratio=', maxi_as
+end program astest
